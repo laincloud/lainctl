e5d03d020d27ddbb879b9738ba8b237b9aa82cf7
lipan <panli7@creditease.cn>
2016-07-26 18:05:19 +0800

    add lainctl network recover cmd, handle network issue when restarting container

---
+++ lain_admin_cli/network.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from argh.decorators import arg, expects_obj
+from lain_admin_cli.helpers import (
+    TwoLevelCommandBase, run_ansible_cmd
+)
+
+
+class Network(TwoLevelCommandBase):
+
+    @classmethod
+    def subcommands(self):
+        return [self.recover]
+
+    @classmethod
+    def namespace(self):
+        return "network"
+
+    @classmethod
+    def help_message(self):
+        return "lain network operations"
+
+    @classmethod
+    @expects_obj
+    @arg('nodename')
+    @arg('-p', '--playbooks', required=True)
+    @arg('-t', '--target_app', required=True, help="The app needing recovery")
+    @arg('-c', '--proc_name', required=True, help="The proc name needing recovery")
+    @arg('-n', '--instance_number', required=True, help='the instance number of proc needing recovery')
+    def recover(self, args):
+        """
+        network recover will fix docker network issues about container endpoint already exist;
+        """
+        run_recovernode_ansible(args)
+
+
+def run_recovernode_ansible(args):
+    envs = {
+        'role': 'network-recover',
+        'recover_node': args.nodename,
+        'recover_app': args.target_app,
+        'recover_proc': args.proc_name,
+        'recover_instance_number': args.instance_number,
+    }
+    return run_ansible_cmd(args.playbooks, envs)
